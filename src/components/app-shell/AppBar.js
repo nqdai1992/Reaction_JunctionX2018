62f33b2ea93868769b8c13fc27fddd384edebff6
dainguyen <nguyendai1992@gmail.com>
2018-10-13 09:03:53 +0700

    update appshell

--- src/components/app-shell/AppBar.js
+++ src/components/app-shell/AppBar.js
@@ -5,7 +5,6 @@ import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import IconButton from "@material-ui/core/IconButton";
-import MenuIcon from "@material-ui/icons/Menu";
 import AccountCircle from "@material-ui/icons/AccountCircle";
 import MenuItem from "@material-ui/core/MenuItem";
 import Menu from "@material-ui/core/Menu";
@@ -46,14 +45,6 @@ class MenuAppBar extends React.Component {
       <div className={classes.root}>
         <AppBar position="static">
           <Toolbar>
-            <IconButton
-              className={classes.menuButton}
-              color="inherit"
-              aria-label="Menu"
-              onClick={this.props.openDrawer}
-            >
-              <MenuIcon />
-            </IconButton>
             <Typography
               variant="title"
               color="inherit"
--- src/layouts/AppShell.js
+++ src/layouts/AppShell.js
@@ -1,29 +1,15 @@
 import React from "react";
 import AppBar from "../components/app-shell/AppBar";
-import AppDrawer from "../components/app-shell/AppDrawer";
+import Routes from "../router/routes";
 
 class AppShell extends React.Component {
-  state = {
-    openDrawer: false
-  };
-  closeDrawer = () => {
-    this.setState({
-      openDrawer: false
-    });
-  };
-  openDrawer = () => {
-    this.setState({
-      openDrawer: true
-    });
-  };
   render() {
     return (
       <div>
         <AppBar openDrawer={this.openDrawer} />
-        <AppDrawer
-          open={this.state.openDrawer}
-          closeDrawer={this.closeDrawer}
-        />
+        <div>
+          <Routes />
+        </div>
       </div>
     );
   }
