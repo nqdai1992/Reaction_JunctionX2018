2720aadd5271ef086e489fee65758bff51e6d547
hungHoangDang <hungdh131@gmail.com>
2018-10-13 09:35:50 +0700

    update profile

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
+
+import UserProfile from './UserProfile/UserProfile';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Hung dep trai
-          </a>
-        </header>
+        <UserProfile />
       </div>
     );
   }
