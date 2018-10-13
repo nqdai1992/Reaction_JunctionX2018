0f7ce0ea31b526ff21e7b25bd838e5dd4bf50e63
Sophie Pham <choe.2292@gmail.com>
2018-10-13 12:21:29 +0700

    promotion list

---
+++ src/components/PromotionList/PromotionCard.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import styled from "styled-components";
+
+const CardContainer = styled.div`
+  display: flex;
+`;
+const Thumbnail = styled.div`
+  flex: 3;
+  height: 128px;
+`;
+
+const CardContent = styled.div`
+  flex: 5;
+`;
+
+const CardAction = styled.div`
+  flex: 2;
+`;
+
+const promotionCard = props => (
+  <CardContainer>
+    <Thumbnail>
+      <img
+        src={require("../../assets/" + props.thumbnail)}
+        alt=""
+        width="128"
+      />
+    </Thumbnail>
+    <CardContent>
+      <h4>{props.name}</h4>
+      <h6>{props.category}</h6>
+      <h3>{props.promotion}</h3>
+    </CardContent>
+    <CardAction>
+      <div>Like</div>
+      <div>Invite</div>
+    </CardAction>
+  </CardContainer>
+);
+
+export default promotionCard;
--- src/store/data.js
+++ src/store/data.js
@@ -1,18 +1,18 @@
 export const promotionList = {
   pro1: {
-    thumbnail: "/restaurant-1.jpg",
+    thumbnail: "services/restaurant-1.png",
     name: "Hutong",
     category: "hotpot, buffet",
     promotion: "Discount 20% for group of 4"
   },
   pro2: {
-    thumbnail: "/restaurant-2.jpg",
+    thumbnail: "services/restaurant-2.jpg",
     name: "Gong cha",
     category: "milk tea",
     promotion: "Buy 4 get 1 free"
   },
   pro3: {
-    thumbnail: "/karaoke-1.jpg",
+    thumbnail: "services/karaoke-1.jpg",
     name: "Just dance karaoke",
     category: "karaoke",
     promotion: "One free beer for each person in groups of 5"
